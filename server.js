04795903db0ced39c511f54bf733607f03e9fa92
greggreg <greggreg@itsgreggreg.com>
2016-05-25 10:18:24 -0500

    fixed all the syntax errors and small bugs

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+// ⇣⇣ Forgot to require bodyParser
+var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
 var path = require('path');
@@ -7,26 +9,31 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+// Parenthesis were mis-matched     this one was forgotten ⇣
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+// ⇣ Both the closing bracket for the anonymous function
+// ⇣ and the closing paren for app.get were forgotten
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
+//⇣ Closing curly bracket for the if block was forgotten
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
-
-app.list(3000, function(){
+//      ⇣ Function is actually called app.listen, not app.list
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
