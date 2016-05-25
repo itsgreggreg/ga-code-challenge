894f34dac1e80a8b6985358a140161710337ce4c
greggreg <greggreg@itsgreggreg.com>
2016-05-25 11:50:10 -0500

    hooked up results

--- public/app.js
+++ public/app.js
@@ -5,19 +5,44 @@
   "use strict";
 
   // First we need to grab references to our app's components
+  let searchForm = document.getElementById("searchForm");
   let searchInput = document.getElementById("searchInput");
   let searchButton = document.getElementById("searchButton");
+  let searchResults = document.getElementById("searchResults");
 
   // Now we attach handlers to events our components may fire
-  searchButton.onclick = function(){
+  searchForm.onsubmit = function(){
     let movieTitle = searchInput.value;
     let successFunction = function(movies){
-      console.log(movies);
+      console.log(movies)
+      searchResults.innerHTML = buildMovieList(movies.Search);
     };
-    let errorFunction = function(){
-      console.log('error');
-    };
-    searchOMDB(movieTitle, successFunction, errorFunction);
+    searchOMDB(movieTitle, successFunction, searchError);
+    return false;
+  }
+
+  // UI FUNCTIONS
+
+  /*
+    Simply displays No Results in the searchResults if called
+  */
+  function searchError(){
+    searchResults.innerHTML = "<h2>No Results</h2>"
+  };
+
+  /*
+    Takes an Array of movies and builds UL to display them
+    Parameters:
+      movies : Array - an array of movie objects returned from OMDB
+    Returns:
+      A UL dom element to be inserted into the web page
+  */
+  function buildMovieList(movies){
+    let ul = '<ul id="movieList">'
+    movies.map(function(movie){
+      ul += '<li>' + movie.Year + ' - ' + movie.Title + '</li>';
+    })
+    return ul + '</ul>'
   }
 
 
@@ -26,11 +51,13 @@
   /*
     A function to search the omdbapi for a specific movie title
     Parameters:
-     - movieTitle : string - the movie title to search for
-     - success : function - a function to call if the request was successful
+     movieTitle : string - the movie title to search for
+     success : function - a function to call if the request was successful
                           receives an array of movie objects as it's only parameter
-     - error : function - a function to call if the request failed
-                          receives no parameters
+     error : function - a function to call if the request failed
+                        receives no parameters
+    Returns:
+      undefined
   */
   function searchOMDB(movieTitle, success, error){
     let baseUrl = "http://omdbapi.com/?s=";
